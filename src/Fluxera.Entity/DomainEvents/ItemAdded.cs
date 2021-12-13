689372a0d96e9b91fa84277a17c4785e74f8eba6
Matthias Gernand <mgernand@users.noreply.github.com>
2021-12-13 04:03:09 +0100

    Added string ID item events. (#5)

--- src/Fluxera.Entity/DomainEvents/ItemAdded.cs
+++ src/Fluxera.Entity/DomainEvents/ItemAdded.cs
@@ -19,4 +19,21 @@
 
 		public TAggregateRoot Item { get; }
 	}
+
+	///  <summary>
+	/// 	A domain event that indicates that a new item of <see cref="AggregateRoot{TAggregateRoot}"/>
+	/// 	has been added to the data storage. This event uses a <see cref="string"/> for the ID type.
+	///  </summary>
+	///  <typeparam name="TAggregateRoot">The aggregate root type.</typeparam>
+	[PublicAPI]
+	public sealed class ItemAdded<TAggregateRoot> : IDomainEvent 
+		where TAggregateRoot : AggregateRoot<TAggregateRoot>
+	{
+		public ItemAdded(TAggregateRoot item)
+		{
+			this.Item = item;
+		}
+
+		public TAggregateRoot Item { get; }
+	}
 }
