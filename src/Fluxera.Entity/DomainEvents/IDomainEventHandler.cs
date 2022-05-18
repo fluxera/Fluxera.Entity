0afad6ca0c0d415d408ac6cb57eea8532e4b2885
Matthias Gernand <mgernand@users.noreply.github.com>
2022-05-18 11:14:21 +0200

    Added marker interface for event handlers. (#17)

--- src/Fluxera.Entity/DomainEvents/IDomainEventHandler.cs
+++ src/Fluxera.Entity/DomainEvents/IDomainEventHandler.cs
@@ -1,25 +1,27 @@
 ﻿namespace Fluxera.Entity.DomainEvents
 {
-	using System;
 	using System.Threading.Tasks;
 	using JetBrains.Annotations;
-	using Microsoft.Extensions.DependencyInjection;
+
+	/// <summary>
+	///     A marker interface for domain event handlers.
+	/// </summary>
+	public interface IDomainEventHandler
+	{
+	}
 
 	/// <summary>
 	///     A contract for implementing domain event handlers, which must be registered
-	///		using dependency injection. Handlers of this type must be executed before
-	///		storing the entity to a storage.
+	///     using dependency injection. Handlers of this type must be executed before
+	///     storing the entity to a storage.
 	/// </summary>
-	/// <remarks>
-	///		See: <see cref="ServiceCollectionExtensions.AddDomainEvents(IServiceCollection,Action{DomainEventHandlerBuilder})"/>.
-	/// </remarks>
 	/// <typeparam name="TDomainEvent">The type of the domain event to handle.</typeparam>
 	[PublicAPI]
-	public interface IDomainEventHandler<in TDomainEvent> 
+	public interface IDomainEventHandler<in TDomainEvent> : IDomainEventHandler
 		where TDomainEvent : class, IDomainEvent
 	{
 		/// <summary>
-		///		Handles the given domain event asynchronously.
+		///     Handles the given domain event asynchronously.
 		/// </summary>
 		/// <param name="domainEvent">The event to handle.</param>
 		Task HandleAsync(TDomainEvent domainEvent);
