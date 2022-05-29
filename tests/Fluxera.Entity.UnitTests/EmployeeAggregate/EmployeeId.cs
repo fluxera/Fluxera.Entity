14e66800d87766bd36b1ad46671a6e6073f21e54
Matthias Gernand <matthias.gernand@fluxera.com>
2022-05-29 23:07:23 +0200

    Use the new strongly-typed IDs library.

---
+++ tests/Fluxera.Entity.UnitTests/EmployeeAggregate/EmployeeId.cs
@@ -0,0 +1,12 @@
+﻿namespace Fluxera.Entity.UnitTests.EmployeeAggregate
+{
+	using Fluxera.StronglyTypedId;
+
+	public sealed class EmployeeId : StronglyTypedId<EmployeeId, string>
+	{
+		/// <inheritdoc />
+		public EmployeeId(string value) : base(value)
+		{
+		}
+	}
+}
